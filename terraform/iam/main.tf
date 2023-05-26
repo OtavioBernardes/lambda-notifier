542db2cde398440b65a2e045a0a8ae56ccdf59ac
Otavio Bernardes <otavio.bernardes@catho.com>
2023-05-25 21:24:25 -0300

    refactor: change policies and roles to json files

--- terraform/iam/main.tf
+++ terraform/iam/main.tf
@@ -2,88 +2,20 @@
 resource "aws_iam_role_policy" "policy_lambda_subscription" {
   name = var.iam.policy_lambda_subscription
   role = aws_iam_role.role_lambda_subscription.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "ec2:CreateNetworkInterface",
-          "ec2:DescribeNetworkInterfaces",
-          "ec2:DeleteNetworkInterface",
-          "ec2:AssignPrivateIpAddresses",
-          "ec2:UnassignPrivateIpAddresses",
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents",
-          "sns:subscribe"
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
+  policy = file("./iam/policies/policy-lambda-subscription.json")
 }
 resource "aws_iam_role" "role_lambda_subscription" {
   name = var.iam.role_lambda_subscription
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        },
-        "Action" : "sts:AssumeRole"
-      },
-    ]
-  })
+  assume_role_policy = file("./iam/roles/role-lambda.json")
 }
 
 # Policy and role to schedule lambda
 resource "aws_iam_role_policy" "policy_lambda_schedule" {
   name = var.iam.policy_lambda_schedule
   role = aws_iam_role.role_lambda_schedule.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "ec2:CreateNetworkInterface",
-          "ec2:DescribeNetworkInterfaces",
-          "ec2:DeleteNetworkInterface",
-          "ec2:AssignPrivateIpAddresses",
-          "ec2:UnassignPrivateIpAddresses",
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents",
-          "scheduler:CreateSchedule",
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
+  policy = file("./iam/policies/policy-lambda-schedule.json")
 }
 resource "aws_iam_role" "role_lambda_schedule" {
   name = var.iam.role_lambda_schedule
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        },
-        "Action" : "sts:AssumeRole"
-      },
-    ]
-  })
+  assume_role_policy = file("./iam/roles/role-lambda.json")
 }
