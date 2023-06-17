3b1642b48f705d494a153db2a0e82baaaf60d4cd
Otavio Bernardes <otavio.bernardes@catho.com>
2023-06-17 20:51:31 -0300

    feat: add bad request returns

--- lambdas/register-subscription/index.js
+++ lambdas/register-subscription/index.js
@@ -1,24 +1,34 @@
 const AWS = require('aws-sdk');
 
-const sns = new AWS.SNS({ apiVersion: '2010-03-31' })
+const sns = new AWS.SNS({ apiVersion: '2010-03-31' });
 
 module.exports.handler = async (event) => {
-  if (!event.email)
-    throw new Error('Missing email in event')
+  const body = JSON.parse(event.body);
+
+  if (!body.email) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'Email is missing' }),
+    };
+  }
 
   const params = {
     Protocol: 'email',
     TopicArn: process.env.SNS_TOPIC_ARN,
-    Endpoint: event.email
+    Endpoint: body.email,
+  };
+
+  try {
+    await sns.subscribe(params).promise();
+    return {
+      statusCode: 201,
+      body: JSON.stringify({ message: "Success: Created subscription" }),
+    };
+  } catch (err) {
+    console.log('Failed to subscribe', err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err }),
+    };
   }
-
-  await sns.subscribe(params, function (err, data) {
-    if (err) {
-      console.log('failed to subscribe', err);
-    } else {
-      console.log('success to subscribe', data)
-    }
-  }).promise()
-
-  return { statusCode: 200 }
-}
+};
--- lambdas/schedule-message/index.js
+++ lambdas/schedule-message/index.js
@@ -1,17 +1,25 @@
 const AWS = require('aws-sdk');
 
 const scheduler = new AWS.Scheduler({ apiVersion: '2021-06-30' });
+const minTimeScheduler = 2
 
 module.exports.handler = (event) => {
-  if (!event.message)
-    throw new Error('Missing message field in event')
+  const body = JSON.parse(event.body);
+  const minutesOffset = body.minutesOffset >= minTimeScheduler ? body.minutesOffset : minTimeScheduler;
 
-  const rule = `new-message-${new Date().getTime()}`
+  if (!body.message) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'Message is missing' }),
+    };
+  }
+
+  const rule = `schedule-new-message-${new Date().getTime()}`;
   const target = {
     RoleArn: process.env.TARGET_ROLE,
     Arn: process.env.SNS_TOPIC_ARN,
     Input: JSON.stringify({
-      message: event.message
+      message: body.message,
     }),
   };
 
@@ -21,31 +29,34 @@ module.exports.handler = (event) => {
       Mode: 'OFF',
     },
     Name: rule,
-    ScheduleExpression: mountScheduleExpression(),
+    ScheduleExpression: mountScheduleExpression(minutesOffset),
     State: 'ENABLED',
     Target: target,
   };
 
   scheduler.createSchedule(params, function (error, data) {
     if (error) {
-      console.log('createSchedule: Error ', { error: JSON.stringify(error) });
+      console.log('createSchedule: Error', { error: JSON.stringify(error) });
       throw error;
     } else {
       console.log(`createSchedule: Success. Data: ${JSON.stringify(data)}`);
     }
   });
 
-  return { statusCode: 200 }
-}
+  return {
+    statusCode: 201,
+    body: JSON.stringify({ message: 'Success: Schedule created' }),
+  };
+};
 
-const mountScheduleExpression = () => {
-  const now = new Date()
-  now.setMinutes(now.getMinutes() + 2);
+const mountScheduleExpression = (minutesOffset) => {
+  const now = new Date();
+  now.setMinutes(now.getMinutes() + minutesOffset);
 
-  const minutes = now.getUTCMinutes() + 2;
-  const hours = now.getUTCHours()
-  const month = now.getUTCMonth() + 1
-  const year = now.getUTCFullYear()
+  const minutes = now.getUTCMinutes();
+  const hours = now.getUTCHours();
+  const month = now.getUTCMonth() + 1;
+  const year = now.getUTCFullYear();
 
-  return `cron(${minutes} ${hours} ${month} ? ${year} ?)`
-}
+  return `cron(${minutes} ${hours} ${month} ? ${year} ?)`;
+};
