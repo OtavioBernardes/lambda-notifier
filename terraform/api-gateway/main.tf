606131ae7ab8e7fd2b062effc36049ddab709558
Otavio Bernardes <otavio.bernardes@catho.com>
2023-06-17 20:51:58 -0300

    feat: remove unnecessary method response

--- terraform/api-gateway/main.tf
+++ terraform/api-gateway/main.tf
@@ -28,30 +28,8 @@ resource "aws_api_gateway_integration" "this" {
   http_method = aws_api_gateway_method.this[each.key].http_method
 
   integration_http_method = "POST"
-  type                    = "AWS"
+  type                    = "AWS_PROXY"
   uri                     = each.value.invoke_arn
-}
-
-resource "aws_api_gateway_method_response" "this" {
-  for_each = { for idx, val in var.api_gateway.resources : idx => val }
-
-  rest_api_id = aws_api_gateway_rest_api.this.id
-  resource_id = aws_api_gateway_resource.this[each.key].id
-  http_method = aws_api_gateway_method.this[each.key].http_method
-  status_code = "200"
-}
-resource "aws_api_gateway_integration_response" "this" {
-  for_each = { for idx, val in var.api_gateway.resources : idx => val }
-
-  rest_api_id = aws_api_gateway_rest_api.this.id
-  resource_id = aws_api_gateway_resource.this[each.key].id
-  http_method = aws_api_gateway_method.this[each.key].http_method
-  status_code = aws_api_gateway_method_response.this[each.key].status_code
-
-  depends_on = [
-    aws_api_gateway_integration.this,
-    aws_api_gateway_integration.this,
-  ]
 }
 resource "aws_api_gateway_deployment" "this" {
   depends_on = [
@@ -61,7 +39,6 @@ resource "aws_api_gateway_deployment" "this" {
   rest_api_id = aws_api_gateway_rest_api.this.id
   stage_name  = var.environment
 }
-
 
 resource "aws_lambda_permission" "this" {
   for_each = { for idx, val in var.api_gateway.resources : idx => val }
